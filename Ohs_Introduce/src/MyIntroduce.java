9e11488eb678a140a742b6a89381d42bbd641762
TJ <TJ@DESKTOP-J2LIGQ3>
2020-02-12 16:49:45 +0900

    현석이 자기소개

---
+++ Ohs_Introduce/src/MyIntroduce.java
@@ -0,0 +1,12 @@
+
+public class MyIntroduce {
+
+	public static void main(String[] args) {
+		
+		System.out.print("�ڽ��� �̸� : ������\n");
+		System.out.print("���� : 27\n");
+		System.out.print("¦���� �ϰ� ���� ��\n");
+		System.out.print("���� ������ ����");
+	}
+
+}
